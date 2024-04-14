51d9f6098a8340676f0f8335bda51be72f314fc6
Solomon Njobvu <solomonnjobvu1@gmail.com>
2024-04-14 23:35:47 +0200

    Dynamic year adaptability

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,13 @@
 export default function Footer(): JSX.Element {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t md:w-[60%] mx-auto pb-8 pt-2 flex flex-col justify-center space-y-2 my-10 text-stone-600 dark:text-stone-400">
       <small className="inline-block text-center">
         built with React, Typescript, Tailwind CSS and Sanity.
       </small>
-      <p className="text-center text-sm w-full">&copy;2023 Solomon Njobvu.</p>
+      <p className="text-center text-sm w-full">
+        &copy;{currentYear} Solomon Njobvu.
+      </p>
     </footer>
   );
 }
