d86549880c714e4c536d9307faed38f52c0d43b9
Solomon Njobvu <solomonnjobvu1@gmail.com>
2024-04-15 20:45:29 +0200

    fixed a bug of a white extra line on mobile

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="border-t md:w-[60%] mx-auto pb-8 pt-2 flex flex-col justify-center space-y-2 my-10 text-stone-600 dark:text-stone-400">
+    <footer className="border-t md:w-[60%] mx-auto pb-8 pt-2 flex flex-col justify-center space-y-2 md:my-10 text-stone-600 dark:text-stone-400">
       <small className="inline-block text-center">
         built with React, Typescript, Tailwind CSS and Sanity.
       </small>
